79a9cffc6ca774f68f1fbf1015c5657f6a6738ee
meerleevven <Emanuel.maunaume@gmail.com>
2025-02-25 15:24:02 +0100

    pricepage.php klaar

--- pricepage.php
+++ pricepage.php
@@ -6,12 +6,49 @@ htmlHead('IMS', 'Prijs pagina');
 <div class="header"></div>
 <div class="banner" id="priceBanner">
         <div class="homepageContainer">
-            
+            <div class="bannerFeature">
+                <div class="bannerFeatures">
+                    <h3>Free &euro;0</h3><p>Small businesses</p>
+                    <hr>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. 
+                    </p>
+                    <hr>
+                    <a href="">Select Plan</a>
+                </div>
+                <div class="bannerFeatures">
+                    <h3>Pro &euro;10</h3><p>Medium businesses</p>
+                    <hr>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. 
+                    </p>
+                    <hr>
+                    <a href="">Select Plan</a>
+                </div>
+                <div class="bannerFeatures">
+                    <h3>Enterprise &euro;20</h3> <p>Large businesses</p>
+                    <hr>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. 
+                    </p>
+                    <hr>
+                    <a href="">Select Plan</a>
+                </div>
+            </div>
         </div>
     </div>
 <div class="homepageContainer" id="container">
-        <div class="homepageFeatures">
-            
+        <div class="sumText">
+            <h1>Why you should choose for the IMS</h1>
+            <p>
+                Lorem ipsum dolor sit amet, conse ctetur adipis cing elit. Nullam euismod, nunc vel 
+                convallis pharetra, justo nulla aliquet metus, a cursus erat velit ut odio. Integer et 
+                arcu sed lacus tincidunt auctor. Fus ce ut ligula ne c magna fermentum auctor. 
+                Suspendisse potenti. Done c auctor, sapien ut rhoncus vehicula, tortor lorem 
+                tincidunt odio, sed malesuada nisi velit in nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia 
+                curae; Aenean at justo sed purus euismod dictum. Morbi tincidunt, tortor et aliquet 
+                sus cipit,
+            </p>
         </div>
     </div>
 <div class="footer">
